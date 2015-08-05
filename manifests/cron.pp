d5d6e411dcc6051ce10bd71ea6a5ba7ff8f78738
Mark McKinstry <mmckinst@nexcess.net>
2015-08-05 13:05:53 -0400

    ksplice::cron

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -1,2 +1,17 @@
-class ksplice::cron {
+class ksplice::cron inherits ksplice {
+
+  # the uptrack package installs /etc/cron.d/uptrack by default but we manage
+  # the cron job with puppet instead
+  file {'/etc/cron.d/uptrack':
+    ensure => 'absent',
+  }
+
+  cron {'ksplice':
+    command  => 'PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin: uptrack-upgrade --cron',
+    minute   => $cron_minute,
+    hour     => $cron_hour,
+    month    => $cron_month,
+    monthday => $cron_monthday,
+    weekday  => $cron_weekday,
+  }
 }
