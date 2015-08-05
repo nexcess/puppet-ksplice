04c2fafe0053a2bc7e11d00329cc482d3c6913ed
Mark McKinstry <mmckinst@nexcess.net>
2015-08-05 12:43:36 -0400

    ksplice::repo

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,2 +1,16 @@
 class ksplice::repo {
+
+  $os = $operatingsystem ? {
+    'CentOS' => 'centos',
+    'RedHat' => 'rhel',
+    'Fedora' => 'fedora',
+  }
+
+  yumrepo {'ksplice-uptrack':
+    ensure   => 'present',
+    baseurl  => "http://www.ksplice.com/yum/uptrack/$os/\$releasever/\$basearch/",
+    enabled  => '1',
+    gpgcheck => '1',
+    gpgkey   => 'https://www.ksplice.com/yum/RPM-GPG-KEY-ksplice',
+  }
 }
