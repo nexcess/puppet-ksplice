4bb4135f064c4ab8f03e49482fc6279fe528a7e1
Mark McKinstry <mmckinst@nexcess.net>
2015-08-05 11:48:19 -0400

    put params in

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,9 +35,25 @@
 #
 # Copyright 2015 Your name here, unless otherwise noted.
 #
-class ksplice inherits ksplice::params {
-  class{'::ksplice::repo':} ->
-  class{'::ksplice::install':} ->
-  class{'::ksplice::config':} ->
-  class{'::ksplice::cron':}
+class ksplice (
+
+  $config_template          = $ksplice::params::config_template,
+  $config_accesskey         = $ksplice::params::config_accesskey,
+  $config_install_on_reboot = $ksplice::params::config_install_on_reboot,
+  $config_upgrade_on_reboot = $ksplice::params::config_upgrade_on_reboot,
+  $config_autoinstall       = $ksplice::params::config_autoinstall,
+
+  $cron_minute              = $ksplice::params::cron_minute,
+  $cron_hour                = $ksplice::params::cron_hour,
+  $cron_month               = $ksplice::params::cron_month,
+  $cron_monthday            = $ksplice::params::cron_monthday,
+  $cron_weekday             = $ksplice::params::cron_weekday,
+
+  ) inherits ksplice::params {
+
+
+    class{'::ksplice::repo':} ->
+    class{'::ksplice::install':} ->
+    class{'::ksplice::config':} ->
+    class{'::ksplice::cron':}
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,2 +1,13 @@
 class ksplice::params {
+  $config_template          = 'ksplice/uptrack.conf.erc'
+  $config_accesskey         = 'INSERT_ACCESS_KEY'
+  $config_install_on_reboot = true
+  $config_upgrade_on_reboot = true
+  $config_autoinstall       = true
+
+  $cron_minute              = [fqdn_rand(30) , fqdn_rand(30) + 30]
+  $cron_hour                = '*'
+  $cron_month               = '*'
+  $cron_monthday            = '*'
+  $cron_weekday             = '*'
 }
