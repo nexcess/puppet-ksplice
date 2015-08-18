7174276d62d1319bcf9df78452b2688920ea7db3
Mark McKinstry <mmckinst@nexcess.net>
2015-08-18 16:10:20 -0400

    only set ksplice facts once uptrack-uname is installed

--- lib/facter/ksplice.rb
+++ lib/facter/ksplice.rb
@@ -1,21 +1,23 @@
-Facter.add(:ksplice_kernelrelease) do
-  setcode '/usr/bin/uptrack-uname -r'
-end
-
-Facter.add(:ksplice_kernelversion) do
-  setcode do
-    Facter.value(:ksplice_kernelrelease).split('-')[0]
+if File.exist?('/usr/bin/uptrack-uname')
+  Facter.add(:ksplice_kernelrelease) do
+    setcode '/usr/bin/uptrack-uname -r'
   end
-end
 
-Facter.add(:ksplice_kernelmajversion) do
-  setcode do
-    Facter.value(:ksplice_kernelversion).split('.')[0..1].join('.')
+  Facter.add(:ksplice_kernelversion) do
+    setcode do
+      Facter.value(:ksplice_kernelrelease).split('-')[0]
+    end
   end
-end
 
-Facter.add(:ksplice_kernel_package_version) do
-  setcode do
-    Facter::Util::Resolution.exec('/usr/bin/uptrack-uname --package-version').split[2]
+  Facter.add(:ksplice_kernelmajversion) do
+    setcode do
+      Facter.value(:ksplice_kernelversion).split('.')[0..1].join('.')
+    end
+  end
+
+  Facter.add(:ksplice_kernel_package_version) do
+    setcode do
+      Facter::Util::Resolution.exec('/usr/bin/uptrack-uname --package-version').split[2]
+    end
   end
 end
